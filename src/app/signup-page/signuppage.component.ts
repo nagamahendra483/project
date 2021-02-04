75bbcadcee5c0812578c57c2985d07dc93fdb4e0
Varakuti, Naga M <naga.m.varakuti@accenture.com>
2021-02-04 11:50:59 +0530

    new changes

--- src/app/signup-page/signuppage.component.ts
+++ src/app/signup-page/signuppage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'login-page',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./signuppage.component.css']
 })
 export class SignupPageComponent {
-  
+  SignUpForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  public SignUp() {
+  }
 }
